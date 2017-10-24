f7a75f7d98b286ef729c172f3386fa489488b70a
Hernawan Fa'iz Abdillah <hernawan.faiz@abdillah.me>
2017-10-24 23:38:00 +0700

    migration: Add create_eatspots_table

---
+++ database/migrations/2017_10_24_162802_create_eatspots_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEatspotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eatspots', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->double('latitude', 15, 10);
+            $table->double('longitude', 15, 10);
+            $table->nullableMorphs('taggable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eatspots');
+    }
+}
