8c785cd64cf46b1cfc43458f278d90893cd59a02
Hanif Sugiyanto <hanifsgy@gmail.com>
2017-11-04 00:21:11 +0700

    Adding materialize laravel.

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,36 +6,90 @@
 @endsection
 
 @section ('page.content')
-    <nav class="navbar fixed-top navbar-dark bg-dark justify-content-between">
-        <a class="navbar-brand">Loco</a>
-        <form class="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Makanan, Minuman, Lalapan" aria-label="Search">
-            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Cari</button>
-        </form>
-    </nav>
-    <div class="jumbotron jumbotron-fluid" style="background-image: url('{{ asset('img/landing-banner-1.jpg') }}'); background-size: cover;">
-        <div class="container">
-            <h1 class="display-3" style="color: #888;">Lapar?</h1>
-            <p class="lead" style="color: #999; text-shadow: 0 0 0 white;"><strong>Cari seleramu di sini!</strong></p>
+    {!! MaterializeCSS::include_full() !!}
+    <link rel="stylesheet" href="http://yourdomain.com/materialize-css/css/materialize.min.css">
+    <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="http://yourdomain.com/materialize-css/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <nav>
+    <div class="nav-wrapper">
+      <div class="brand-logo center">LOCO</div>
+      <form>
+        <div class="input-field">
+          <input id="search" type="search" required>
+          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
         </div>
+      </form>
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="alert alert-warning">
-                    Under Heavy Construction<br/>
-                    <small>Loco Hunter - Food Edition</small>
-                </div>
+  </nav>
 
 
+<ul class="collection with-header">
+    <li class="collection-header"><h8>Jenis Makanan</h8></li>
+    <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="fastfood" src="https://upload.wikimedia.org/wikipedia/commons/2/2e/Fast_food_meal.jpg">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">Makan Cepat<i class="material-icons right">more_vert</i></span>
+                <p><a href="#">Cari</a></p>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Makan Cepat<i class="material-icons right">close</i></span>
+                <p>Makan cepat, adalah berbagai jenis makanan yang dapat di santap dalam kurun waktu 10 - 15 menit.</p>
+            </div>
+    </div>
+     <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+             <img class="activator" src="http://gldelapan.com/images/WATERMARK/201603/20053/masakan-indonesia-nasi-bakso-telur.jpg">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">Makan Sedang<i class="material-icons right">more_vert</i></span>
+                <p><a href="#">Cari</a></p>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Makan Sedang<i class="material-icons right">close</i></span>
+                <p>Makan sedang, adalah berbagai jenis makanan yang dapat di santap dalam kurun waktu 15 - 20 menit.</p>
             </div>
         </div>
-    </div>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col col-offset-1">
-                Copyright (c) 2020 <a href="https://locohunter.com/">Locomotive</a>
+     <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+             <img class="activator" src="https://nacelopendoor.files.wordpress.com/2017/03/blog-5-2.jpg?w=736">
             </div>
-        </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">Makan Lama<i class="material-icons right">more_vert</i></span>
+                <p><a href="#">Cari</a></p>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Makan Lama<i class="material-icons right">close</i></span>
+                <p>Makan lama, adalah berbagai jenis makanan yang dapat di santap dalam kurun waktu yang lebih dari 20 menit.</p>
+            </div>
     </div>
+</ul>
+
+
+
+
+
+    <style>
+        .card {
+            min-height: 150px;
+            min-width: 50%;
+            height: 250px;
+            width:auto;
+        }
+
+        .fastfood {
+            min-height: 100px;
+            min-width: 50%;
+            height: auto;
+            width:auto;
+        }
+
+    </style>
+
 @endsection
+
+
+
