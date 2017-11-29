72a6a4910493e7e655bf8b3127582f35085c8580
Hernawan Fa'iz Abdillah <hernawan.faiz@abdillah.me>
2017-11-29 21:21:40 +0700

    route: Add basic foundation for data flow

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
 <?php
+use Illuminate\Http\Request;
+use App\Models\Eatspot;
+use App\Models\Foodstuff;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,14 +14,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/', function (Request $request) {
+    $mode = $request->input('mode');
+    return view('home', [ 'searchMode' => $mode ]);
 });
 
-Route::get('/search', function () {
-    return view('welcome');
+Route::get('/search', function (Request $request) {
+    $q = $request->input('q');
+
+    // Query database for foods
+    $foods = Foodstuff::where('name', 'LIKE', "%$q%")
+        ->orWhere('description', 'LIKE', "%$q%");
+
+    return view('search', [ 'foodstuffs' => $foods ]);
 });
 
-Route::get('/eatspot/:id', function () {
-    return view('welcome');
+Route::get('/eatspot/{id}', function ($id) {
+    $eatspot = Eatspot::find($id);
+    return view('eatspot', [ 'eatspot' => $eatspot ]);
 });
